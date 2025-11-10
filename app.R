309afad7d09482c6730cb245667ddb912ce391c2
Aaron Gorman <gormanaa@tcd.ie>
2025-11-10 14:56:05 +0000

    fix:all

--- app.R
+++ app.R
@@ -1048,10 +1048,10 @@ document.addEventListener('shown.bs.tab', function (event) {
                                             div(id = "stroke", class = "pt-5", h2("Stroke")),
                                             div(id = "stroke_banner",ui_reactable, style = 'overflow:visible;width:60vw;padding-top:100px;padding-bottom:50px;font-size:0.7rem;'),
                                             div(id = "stroke_age", class = "pt-5", h4("Age")),
-                                            # echarts4rOutput('stroke_age20'),
+                                            echarts4rOutput('stroke_age20'),
                                             lapply(stroke_HSCT[[3]],print),
                                             # stroke_age20,
-
+                                            stroke_HSCT[[3]][[1]][[5]],
                                             # lapply(stroke_age20[[1]],print),
                                             # stroke_age20,
                                             echarts4rOutput("dummy", height = "100px"),
@@ -1627,10 +1627,10 @@ $(document).ready(function() {
 
 server <- function(input, output, session) {
 
-  # output$stroke_age20 <- renderEcharts4r({
-  #   stroke_age20 |>
-  #     e_theme('roma')
-  #   })
+  output$stroke_age20 <- renderEcharts4r({
+    stroke_age20 |>
+      e_theme('roma')
+    })
 
   output$dummy <- renderEcharts4r({
     e_charts(mtcars ,cyl) %>%
